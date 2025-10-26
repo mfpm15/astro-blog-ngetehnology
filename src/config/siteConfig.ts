c1f71c3e4208fc36ea9d78dac3a7e7f0fe4a6148
mfpm15 <faiz1503@hotmail.com>
2025-10-27 07:05:01 +0800

    Improve Netlify Identity flow

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -67,7 +67,7 @@ export const siteConfig: SiteConfig = {
   // Konfigurasi tata letak daftar postingan
   postListLayout: {
     // Mode tata letak default: "list" mode daftar (tata letak satu kolom), "grid" mode petak (tata letak dua kolom)
-    defaultMode: "grid",
+    defaultMode: "list",
     // Apakah mengizinkan pengguna untuk beralih tata letak
     allowSwitch: true,
   },
